350d0ae480b90e28b37c34ddff3623afbdfaae77
veeking <445915818@qq.com>
2015-07-21 23:45:20 +0800

    游戏开发入门_弹弓游戏

--- game/froot wars/common/game.js
+++ game/froot wars/common/game.js
@@ -104,18 +104,56 @@ var game = {
         game.handlePanning();
 
         //移动角色
+        var currentTime = new Date().getTime();
+        var timeStep;
+        if(game.lastTime){
+           timeStep = (currentTime - game.lastTime) / 1000;  //  毫秒换成 秒
+           box2d.step(timeStep);  // 取差作为timeStep
+        }
+        game.lastTime = currentTime;
+
+
 
         //视差滚动绘制背景
         game.context.drawImage(game.currentLevel.backgroundImage,game.offsetLeft/4,0,640,480,0,0,640,480); //game.offsetLeft/4表示X坐标不断移动
         game.context.drawImage(game.currentLevel.foregroundImage,game.offsetLeft,0,640,480,0,0,640,480);
         // 绘制弹弓
-
         game.context.drawImage(game.slingshotImage,game.slingshotX - game.offsetLeft,game.slingshotY);
+        // 绘制所有物体
+        game.drawAllBodies();
+        // 绘制前置弹弓，为了实现 遮挡英雄 效果
         game.context.drawImage(game.slingshotFrontImage,game.slingshotX - game.offsetLeft,game.slingshotY);
-            console.log('animate runing')
+        console.log('animate runing')
+
         if(!game.ended){  // 如果没有结束  则 继续调用动画
             game.animationFrame = window.requestAnimationFrame(game.animate,game.canvas);
         }
+    },
+    drawAllBodies:function(){
+        box2d.world.DrawDebugData();
+        //  遍历所有物体，并在游戏画布上绘制出来
+        for(var body = box2d.world.GetBodyList();body;body = body.GetNext()){
+            var entity = body.GetUserData();
+            if(entity){
+                entities.draw(entity,body.GetPosition(),body.GetAngle());
+            }
+        }
+
+    },
+    countEntities:function(){  // 统计英雄和坏蛋的数量
+        game.heroes = [];
+        game.villains = [];
+        for(var body = box2d.world.GetBodyList();body;body = body.GetNext()){
+            var entity = body.GetUserData();
+            if(entity){
+                if(entity.type == "hero"){
+                    game.heroes.push(body);
+                }else if(entity.type == "villain"){
+                    game.villains.push(body);
+                }
+            }
+        }
+        console.log(game.heroes)
     },
     handlePanning:function(){
         if(game.mode == "intro"){  // 关卡刚刚载入
@@ -133,13 +171,31 @@ var game = {
         } // end wait-for-firing
 
         if(game.mode == "load-next-hero"){  // 检查是否有下一个英雄装弹弓
-           //待完成
+           game.countEntities();
            // 检查是否有坏蛋活着,如果没有，结束关卡(通过)
+            if(game.villains.length == 0){
+               game.mode = "level-success";
+                return;
+            }
+            if(game.heroes.length == 0){
+                game.mode = "level-failure";
+                return;
+            }
            // 检查是否还有可装填的英雄，如果没有，结束关卡(失败)
            // 装填英雄，设置状态到 wait-for-firing
-            game.mode = "wait-for-firing";
+            if(!game.currentHero){
+                game.currentHero = game.heroes[game.heroes.length-1];
+                game.currentHero.SetPosition({x:180/box2d.scale,y:200/box2d.scale});
+                game.currentHero.SetLinearVelocity({x:0,y:0});
+                game.currentHero.SetAngularVelocity(0);
+                game.currentHero.SetAwake(true);
+            }else{
+                game.panTo(game.slingshotX);
+                if(!game.currentHero.IsAwake()){
+                    game.mode = "wait-for-firing";
+                }
+            }
         } // end load-next-hero
-
         if(game.mode == "firing"){  // 已单击英雄，鼠标没释放和还没拖拽
            game.panTo(game.slingshotX);
         } // end firing
@@ -149,6 +205,8 @@ var game = {
         } // end fired
     },
     panTo : function(newCenter){ //画面中心移动到newCenter
+
+        //移回中心点
         if(Math.abs(newCenter - game.offsetLeft - (game.canvas.width/4)) > 0 && game.offsetLeft <= game.maxOffset && game.offsetLeft >= game.minOffset){
             //  以下条件未被满足的话 继续平移
             // 1、当前offset如果没超过中心点的1/4
@@ -156,7 +214,6 @@ var game = {
             var deltaX = Math.round((newCenter - game.offsetLeft - (game.canvas.width/4)) / 2);  // 让画布平移逐渐加快， 递增的速度变量
             if(deltaX && Math.abs(deltaX) > game.maxSpeed){  // 如果递增的速度大于最大速度值，则让其等于速度值
                 deltaX = game.maxSpeed * Math.abs(deltaX) / (deltaX);
-//                  deltaX = game.maxSpeed;
             }
             game.offsetLeft += deltaX;
 
@@ -164,6 +221,7 @@ var game = {
             return true;
         }  // end Math.abs
 
+        // 边缘检测
         if(game.offsetLeft < game.minOffset){  //检测平移是否超出范围
             game.offsetLeft = game.minOffset;
             return true;
@@ -181,12 +239,46 @@ var levels = { //  关卡对象
         {  //  第一关
            foreground : 'desert-foreground',
            background:'clouds-background',
-           entities:[]
+           entities:[
+               {type : "ground",name : "dirt",x:500,y:440,width:1000,height:20,isStatic:true},
+               {type : "ground",name : "wood",x:180,y:390,width:40,height:80,isStatic:true},
+
+               {type : "block",name : "wood",x:520,y:380,angle:90,width:100,height:25},
+               {type : "block",name : "glass",x:520,y:280,angle:90,width:100,height:25},
+               {type : "villain",name : "burger",x:520,y:205,calories:590},
+
+               {type : "block",name : "wood",x:620,y:380,angle:90,width:100,height:25},
+               {type : "block",name : "glass",x:620,y:280,angle:90,width:100,height:25},
+               {type : "villain",name : "fries",x:620,y:205,calories:420},
+
+               {type : "hero",name : "orange",x:60,y:405},
+               {type : "hero",name : "apple",x:120,y:405},
+           ]
         },
         {  //  第二关
             foreground : 'desert-foreground',
             background:'clouds-background',
-            entities:[]
+            entities:[
+                {type : "ground",name : "dirt",x:500,y:440,width:1000,height:20,isStatic:true},  // 弹弓和地面
+                {type : "ground",name : "wood",x:210,y:390,width:40,height:80,isStatic:true},
+
+                {type : "block",name : "wood",x:820,y:380,angle:90,width:100,height:25},
+                {type : "block",name : "wood",x:720,y:380,angle:90,width:100,height:25},
+                {type : "block",name : "wood",x:620,y:380,angle:90,width:100,height:25},
+                {type : "block",name : "glass",x:670,y:255,angle:90,width:100,height:25},
+                {type : "block",name : "glass",x:770,y:255,angle:90,width:100,height:25},
+                {type : "block",name : "glass",x:670,y:318,width:100,height:25},
+                {type : "block",name : "glass",x:770,y:318,width:100,height:25},
+                {type : "block",name : "wood",x:720,y:193,width:100,height:25},
+
+                {type : "villain",name : "burger",x:715,y:155,calories:590},
+                {type : "villain",name : "burger",x:670,y:405,calories:420},
+                {type : "villain",name : "burger",x:765,y:405,calories:150},
+
+                {type : "hero",name : "strawberry",x:25,y:415,calories:420},
+                {type : "hero",name : "orange",x:75,y:405,calories:410},
+                {type : "hero",name : "apple",x:135,y:405,calories:410},
+            ]
         },
     ],
     init : function(){
@@ -204,6 +296,8 @@ var levels = { //  关卡对象
         });
     },
     load:function(num){
+        // 关卡加载时 ，初始化box2d世界
+        box2d.init();
         //声明新的当前关卡对象
         game.currentLevel = {number:num,hero:[]};  // 保存已加载的关卡数据
         game.score = 0;
@@ -216,6 +310,11 @@ var levels = { //  关卡对象
         game.slingshotImage = loader.loadImage('./img/slingshot.png'); // 加载弹弓图像
         game.slingshotFrontImage = loader.loadImage('./img/slingshot-front.png'); // 加载弹弓图像
         //一旦所有的资源加载完成,调用start开始游戏
+        //加载所有物体
+        for(var i=level.entities.length - 1;i >= 0;i--){
+            var entity = level.entities[i];
+            entities.create(entity);
+        };
 
          loader.onload = game.start;  //因为资源加载是异步操作，所以需要 定义onload 方法为game.start的回调方法
             //  某个时刻(资源加载完成的时候)调用 game.start
@@ -253,4 +352,139 @@ var mouse = {    // 处理各种鼠标事件的对象
         mouse.dragging = false;
     }
 
-}
+};
+
+var entities = {  // 定义物体类型, 物体(玻璃、木材和地面)、英雄和坏蛋(橙子、苹果和汉堡)
+    definitions:{
+        "glass":{
+            fullHealth:100,
+            density:2.4,
+            friction:0.4,
+            restitution:0.15
+        },
+        "wood":{
+            fullHealth:500,
+            density:0.7,
+            friction:0.4,
+            restitution:0.4
+        },
+        "dirt":{
+            density:3.0,
+            friction:1.5,
+            restitution:0.2
+        },
+        "burger":{
+            shape:"circle",
+            fullHealth:40,
+            radius:25,
+            density:1,
+            friction:0.5,
+            restitution:0.4
+        },
+        "sodacan":{
+            shape:"rectangle",
+            fullHealth:80,
+            width:40,
+            height:50,
+            density:1,
+            friction:0.5,
+            restitution:0.7
+        },
+        "fries":{
+            shape:"rectangle",
+            fullHealth:50,
+            width:40,
+            height:50,
+            density:1,
+            friction:0.5,
+            restitution:0.6
+        },
+        "apple":{
+            shape:"circle",
+            radius:25,
+            density:1.5,
+            friction:0.5,
+            restitution:0.4
+        },
+        "orange":{
+            shape:"circle",
+            radius:25,
+            density:1.5,
+            friction:0.5,
+            restitution:0.4
+        },
+        "strawberry":{
+            shape:"circle",
+            radius:15,
+            density:2.0,
+            friction:0.5,
+            restitution:0.4
+        }
+    },
+    create : function(entity){ // 以物体作为参数，创建一个Box2d物体，并加入世界
+          var definition = entities.definitions[entity.name];
+          if(!definition){
+              console.log('未知的实体名');
+              return;
+          };
+        switch(entity.type){
+            case 'block' : // 简单的矩形
+               entity.health = definition.fullHealth;
+               entity.fullHealth = definition.fullHealth;
+               entity.shape = "rectangle";
+               entity.sprite = loader.loadImage("./img/entities/" + entity.name + ".png");
+               box2d.createRectangle(entity,definition);
+               break;
+            case 'ground' : // 简单的矩形  : 地面
+                // 不可摧毁的物体 ， 无生命体
+               entity.shape = "rectangle";
+                // 不会被画出，所以不具有图像
+               box2d.createRectangle(entity,definition);
+               break;
+            case 'hero' : // 简单的圆
+            case 'villain' : // 可以是圆形或是矩形 : 坏蛋
+                entity.health = definition.fullHealth;
+                entity.fullHealth = definition.fullHealth;
+                entity.shape = definition.shape;
+                entity.sprite = loader.loadImage("./img/entities/" + entity.name + ".png");
+
+                if(definition.shape == "circle"){
+                    entity.radius = definition.radius;
+                    box2d.createCircle(entity,definition);
+                }else if(definition.shape == "rectangle"){
+                    entity.width = definition.width;
+                    entity.height = definition.height;
+                    box2d.createRectangle(entity,definition);
+                }
+                break;
+            default :
+                console.log('未知的实体');
+                break;
+        };
+    },
+    draw : function(entity,position,angle){// 以物体、物体的位置和角度作为参数，在画面中绘制物体 (重点和难点)
+            // 以物体及其位置、角度为参数，将其绘制在游戏canvas上
+          game.context.translate(position.x * box2d.scale - game.offsetLeft,position.y * box2d.scale);
+          game.context.rotate(angle);  // 将 绘图环境 平移并旋转到物体的位置和角度
+          switch(entity.type){  // 根据物体的类型和形状将其在canvas上绘制出来
+              case "block" :
+                  game.context.drawImage(entity.sprite,0,0,entity.sprite.width,entity.sprite.height,-entity.width/2-1,-entity.height/2-1,entity.width+2,entity.height+2);
+                  break;
+              case "villain":
+              case "hero" :
+                  if(entity.shape == "circle"){
+                      game.context.drawImage(entity.sprite,0,0,entity.sprite.width,entity.sprite.height,-entity.radius-1,-entity.radius-1,entity.radius*2+2,entity.radius*2+2);
+                  }else if(entity.shape == "rectangle"){
+                      game.context.drawImage(entity.sprite,0,0,entity.sprite.width,entity.sprite.height,-entity.width/2-1,-entity.height/2-1,entity.width+2,entity.height+2);
+                  } // end if
+                  break;
+              case "ground":
+                  // 什么都不做  我们单独绘制地面和弹弓
+                  break;
+          } // end swith
+        //最后，将绘图环境逆时针旋转至初始位置
+        game.context.rotate(-angle);
+        game.context.translate(-position.x*box2d.scale + game.offsetLeft,-position.y*box2d.scale);
+    }
+};
+
