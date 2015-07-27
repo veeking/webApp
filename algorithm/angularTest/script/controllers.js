22fee14d5dcee037c04f94af791a7a472b016a98
King Wang <445915818@qq.com>
2015-07-27 23:18:38 +0800

    angular wth json test

--- algorithm/angularTest/script/controllers.js
+++ algorithm/angularTest/script/controllers.js
@@ -1,14 +1,28 @@
 /**
  * Created by veeking on 2015/7/27.
  */
-  testApp.controller('listCtrl',function($scope){
+  testApp.controller('listCtrl',function($scope,$http){
+      $http.get('json/list.json').success(function(data){
+          $scope.newObjs = data;
+      });
 
   });
-testApp.controller('detailCtrl',function($scope,$routeParams){
-    $scope.id = $routeParams.id;
-});
-
-
- testApp.service('getJson',function($http){
-
+ testApp.controller('detailCtrl',function($scope,$routeParams){
+     $scope.newList = {};
+     $scope.newList.id = $routeParams.id;
  });
+
+
+// testApp.factory('getJson',function($http){
+//      return {
+//          newsData : function(){
+//              var oData = {};
+//              $http.get('json/list.json').success(function(data){
+//                  oData = data;
+//                  console.log("se")
+//                  return oData;
+//              });
+//          }
+//      }
+//
+// });
